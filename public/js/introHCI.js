d2deaae7ca6327182dc40410e468ac1250d3a42c
Mikaela Grenion <mgrenion@ucsd.edu>
2021-02-25 11:09:51 -0800

    commit

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,15 +10,25 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
+ 	initCamera();
+ 	initMap();
  	initGestures();
  	initRSVPForm();
 }
 
 // init jQuery gestures  
 function initGestures() {
-	// add gestures listener here
+	$(function(){
+
+		$(".judge-img").bind("taphold",tapholdHandler);
+
+		function tapholdHandler(event){
+			var targetIDPrefix=event.target.id;
+			console.log("got prefix: "+targetIDPrefix);
+			$("#"+targetIDPrefix+"-bio").show();
+
+		}
+	});
 }
 
 // init RSVP form submit listener
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'G3FzUKGyA2LLQ0ZMG2sveKtDbWkA0xjK';
+
+// 'map' refers to a <div> element with the ID map
+var map=L.mapquest.map('map', {
+  center: [37.7749, -122.4194],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 12,
+  zoomControl: false
+	});
+
+	L.marker([32.88,-117.236]).addTo(map);
 }
